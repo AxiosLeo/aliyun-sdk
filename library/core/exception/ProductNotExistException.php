ba0065860d4be388b642072e9cc20c8e97161855
axios <axioscros@aliyun.com>
2018-02-08 11:20:13 +0800

    Product not exist exception

---
+++ library/core/exception/ProductNotExistException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/2/8 09:31
+ */
+
+namespace aliyun\sdk\core\exception;
+
+
+use Throwable;
+
+class ProductNotExistException extends \RuntimeException
+{
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
