45697304ed50381eb052a4fb39b7fa17f4b248a7
axios <axioscros@aliyun.com>
2018-02-08 11:23:59 +0800

    xml to array

---
+++ library/core/help/Parse.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/2/7 17:15
+ */
+
+namespace aliyun\sdk\core\help;
+
+
+class Parse
+{
+    public static function xmlToArray($xml)
+    {
+        //禁止引用外部xml实体
+        libxml_disable_entity_loader(true);
+        $values = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+        return $values;
+    }
+}
