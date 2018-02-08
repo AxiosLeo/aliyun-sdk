1e2d10151b0557ce17f3e8a2aa615c16b4d7410e
axios <axioscros@aliyun.com>
2018-02-08 11:22:18 +0800

    sign maker

--- library/core/sign/HmacSHA1.php
+++ library/core/sign/HmacSHA1.php
@@ -10,6 +10,15 @@
 namespace aliyun\sdk\core\sign;
 
 
-class HmacSHA1{
+use aliyun\sdk\Aliyun;
 
+class HmacSHA1 extends Signature {
+    public static function create($param){
+        ksort($param);
+        $param_string = http_build_query($param);
+        $param_string = urlencode($param_string);
+        $signature = base64_encode(hash_hmac('sha1', $param_string, Aliyun::$access_secret, true));
+        $signature = urlencode($signature);
+        return $signature;
+    }
 }
