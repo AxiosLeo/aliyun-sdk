7689893a3ccf9de6a7564cf6db23bb1847e662a1
axios <axioscros@aliyun.com>
2018-02-08 11:24:41 +0800

    loader

---
+++ library/core/loader.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/2/7 17:00
+ */
+namespace aliyun\sdk\core;
+
+class loader
+{
+
+}
