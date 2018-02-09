93cbc867cfccddc21b61f6bb838b21fc538ce801
axios <axioscros@aliyun.com>
2018-02-09 15:56:53 +0800

    阿里云接口返回的结果处理类

---
+++ library/core/http/Response.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/2/9 13:36
+ */
+
+namespace aliyun\sdk\core\http;
+
+
+class Response
+{
+    public $body;
+
+    public $status;
+
+    public function setBody($body){
+        $this->body = $body;
+    }
+
+    public function setStatus($status){
+        $this->status = $status;
+    }
+
+    public function getContent($format = "json"){
+        if($format === "json"){
+            $result = json_decode($this->body,true);
+            return empty($result) ? $this->body : $result;
+        }
+        return $this->body;
+    }
+}
