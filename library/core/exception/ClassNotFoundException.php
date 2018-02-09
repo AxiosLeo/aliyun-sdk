5860c70c1dfbdecb06ac082c5e51fb3d4282eb98
axios <axioscros@aliyun.com>
2018-02-09 16:26:09 +0800

    类不存在的异常抛出

---
+++ library/core/exception/ClassNotFoundException.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/2/9 16:20
+ */
+
+namespace aliyun\sdk\core\exception;
+
+
+class ClassNotFoundException extends \RuntimeException
+{
+    protected $class;
+    public function __construct($message, $class = '')
+    {
+        $this->message = $message;
+        $this->class   = $class;
+    }
+
+    /**
+     * 获取类名
+     * @access public
+     * @return string
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+}
