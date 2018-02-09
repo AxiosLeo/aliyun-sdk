b7db626637f86a5860d93a3346e37ac6c28bb5db
ydtec-hx <369017150@qq.com>
2018-02-09 13:27:21 +0800

    client exception

---
+++ library/core/exception/ClientException.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @author: Axios
+ *
+ * @email: axioscros@aliyun.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/2/8 16:35
+ */
+
+namespace aliyun\sdk\core\exception;
+
+
+class ClientException extends \Exception
+{
+    public function __construct($errorMessage, $errorCode)
+    {
+        parent::__construct($errorMessage);
+        $this->errorMessage = $errorMessage;
+        $this->errorCode = $errorCode;
+        $this->setErrorType("Client");
+    }
+
+    private $errorCode;
+    private $errorMessage;
+    private $errorType;
+
+    public function getErrorCode()
+    {
+        return $this->errorCode;
+    }
+
+    public function setErrorCode($errorCode)
+    {
+        $this->errorCode = $errorCode;
+    }
+
+    public function getErrorMessage()
+    {
+        return $this->errorMessage;
+    }
+
+    public function setErrorMessage($errorMessage)
+    {
+        $this->errorMessage = $errorMessage;
+    }
+
+    public function getErrorType()
+    {
+        return $this->errorType;
+    }
+
+    public function setErrorType($errorType)
+    {
+        $this->errorType = $errorType;
+    }
+}
