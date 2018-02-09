814858416fe9311ebfd8b9f52632be0c996d1623
axios <axioscros@aliyun.com>
2018-02-09 15:57:50 +0800

    Hmac-sha1加密生成签名

--- library/core/sign/HmacSHA1.php
+++ library/core/sign/HmacSHA1.php
@@ -9,16 +9,39 @@
 
 namespace aliyun\sdk\core\sign;
 
-
 use aliyun\sdk\Aliyun;
 
 class HmacSHA1 extends Signature {
-    public static function create($param){
-        ksort($param);
-        $param_string = http_build_query($param);
-        $param_string = urlencode($param_string);
-        $signature = base64_encode(hash_hmac('sha1', $param_string, Aliyun::$access_secret, true));
-        $signature = urlencode($signature);
+    public static $param_string;
+
+    public static function create($param, $method = "POST"){
+        $param_string = self::paramString($param,$method);
+        self::$param_string = $param_string;
+        $signature = self::makeSign(Aliyun::$access_secret."&",$param_string);
         return $signature;
+    }
+
+    private static function paramString($param , $method){
+        //按参数名排序
+        ksort($param);
+
+        $param_string = "";$n=0;
+        foreach ($param as $k=>$v){
+            //对参数名称和参数值进行 URL 编码
+            $k = urlencode($k);
+            $v = urlencode($v);
+            //对编码后的参数名称和值使用英文等号（=）进行连接
+            if($n!=0){
+                $param_string .= "&";
+            }
+            $param_string .= $k."=".$v;
+            $n++;
+        }
+
+        return $method."&".urlencode("/")."&".urlencode($param_string);
+    }
+
+    public static function makeSign($key = "",$string = ""){
+        return base64_encode(hash_hmac('sha1', $string, $key, true));
     }
 }
