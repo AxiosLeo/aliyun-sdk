4fdfe12d6e99508872b751659795f8f63fd031d4
axios <axioscros@aliyun.com>
2018-02-24 10:12:03 +0800

    获取视频播放凭证

---
+++ library/vod/request/Video/GetVideoPlayAuth.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/2/24 09:54
+ */
+
+namespace aliyun\sdk\vod\request\Video;
+
+
+class GetVideoPlayAuth
+{
+
+}
