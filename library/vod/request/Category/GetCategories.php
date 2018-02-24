33b0a4c1b8ebb348563ed18e79da227c2622c5cc
axios <axioscros@aliyun.com>
2018-02-24 10:28:33 +0800

    获取视频分类

---
+++ library/vod/request/Category/GetCategories.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/2/24 09:25
+ */
+
+namespace aliyun\sdk\vod\request\Category;
+
+use aliyun\sdk\core\traits\Request;
+use aliyun\sdk\vod\request\VodCommon;
+
+/**
+ * Class Get
+ * @package aliyun\sdk\vod\request\Category
+ * @method $this setCateId($cateId)
+ */
+class GetCategories extends VodCommon
+{
+    use Request;
+}
