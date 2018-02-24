5ffae4a46e068ce4f18fdc4b2bea8d0d63501bd8
axios <axioscros@aliyun.com>
2018-02-24 10:12:53 +0800

    获取视频信息

---
+++ library/vod/request/Video/GetInfo.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/2/24 09:09
+ */
+namespace aliyun\sdk\vod\request\Video;
+
+use aliyun\sdk\core\traits\Request;
+use aliyun\sdk\vod\request\VodCommon;
+
+/**
+ * Class GetInfo
+ * @package aliyun\sdk\vod\request\Video
+ * @method $this setVideoId($video_id)
+ */
+class GetInfo extends VodCommon
+{
+    use Request;
+}
