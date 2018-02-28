35a2924c2c983ded83de42afe4561b90b127cb85
axios <axioscros@aliyun.com>
2018-02-28 16:28:21 +0800

    请求方式

---
+++ library/core/help/HttpMethod.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/2/28 13:56
+ */
+
+namespace aliyun\sdk\core\help;
+
+
+class HttpMethod
+{
+    const GET    = 'GET';
+    const PUT    = 'PUT';
+    const POST   = 'POST';
+    const HEAD   = 'HEAD';
+    const DELETE = 'DELETE';
+}
