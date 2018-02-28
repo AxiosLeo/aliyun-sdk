f7264948ca09c0e2944922c18b71b0c4dfab4691
axios <axioscros@aliyun.com>
2018-02-28 16:28:06 +0800

    数组转xml

--- library/core/help/Parse.php
+++ library/core/help/Parse.php
@@ -23,4 +23,49 @@ class Parse
     public static function parseEnableParam($enable){
         return in_array($enable,["1",1,"On","on","ON"]) ? "On":"Off";
     }
+
+    public static function arrayToXml($data, $rootNodeName = 'data', $xml=null)
+    {
+        // turn off compatibility mode as simple xml throws a wobbly if you don't.
+        if (ini_get('zend.ze1_compatibility_mode') == 1)
+        {
+            ini_set ('zend.ze1_compatibility_mode', 0);
+        }
+
+        if ($xml == null)
+        {
+            $xml = simplexml_load_string("<?xml version='1.0' encoding='UTF-8'?><$rootNodeName />");
+        }
+
+        // loop through the data passed in.
+        foreach($data as $key => $value)
+        {
+            // no numeric keys in our xml please!
+            if (is_numeric($key))
+            {
+                // make string key...
+                $key = "unknownNode_". (string) $key;
+            }
+
+            // replace anything not alpha numeric
+            $key = preg_replace('/[^a-z]/i', '', $key);
+
+            // if there is another array found recrusively call this function
+            if (is_array($value))
+            {
+                $node = $xml->addChild($key);
+                // recrusive call.
+                self::arrayToXml($value, $rootNodeName, $node);
+            }
+            else
+            {
+                // add single node.
+                $value = htmlentities($value);
+                $xml->addChild($key,$value);
+            }
+
+        }
+        // pass back as string. or simple xml object if you want!
+        return $xml->asXML();
+    }
 }
