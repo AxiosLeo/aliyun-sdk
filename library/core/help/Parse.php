5f00a343a56eca2a931ede36f59f65f916b8542e
axios <axioscros@aliyun.com>
2018-03-01 14:43:56 +0800

    获取bucket信息

--- library/core/help/Parse.php
+++ library/core/help/Parse.php
@@ -14,58 +14,24 @@ class Parse
 {
     public static function xmlToArray($xml)
     {
-        //禁止引用外部xml实体
         libxml_disable_entity_loader(true);
         $values = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
         return $values;
+    }
+
+    public static function arrayToXml($data, $rootNodeName = 'data')
+    {
+        $xml = new ArrayToXML();
+        $xml_content = $xml->buildXML($data, $rootNodeName);
+        return $xml_content."\n";
     }
 
     public static function parseEnableParam($enable){
         return in_array($enable,["1",1,"On","on","ON"]) ? "On":"Off";
     }
 
-    public static function arrayToXml($data, $rootNodeName = 'data', $xml=null)
-    {
-        // turn off compatibility mode as simple xml throws a wobbly if you don't.
-        if (ini_get('zend.ze1_compatibility_mode') == 1)
-        {
-            ini_set ('zend.ze1_compatibility_mode', 0);
-        }
-
-        if ($xml == null)
-        {
-            $xml = simplexml_load_string("<?xml version='1.0' encoding='UTF-8'?><$rootNodeName />");
-        }
-
-        // loop through the data passed in.
-        foreach($data as $key => $value)
-        {
-            // no numeric keys in our xml please!
-            if (is_numeric($key))
-            {
-                // make string key...
-                $key = "unknownNode_". (string) $key;
-            }
-
-            // replace anything not alpha numeric
-            $key = preg_replace('/[^a-z]/i', '', $key);
-
-            // if there is another array found recrusively call this function
-            if (is_array($value))
-            {
-                $node = $xml->addChild($key);
-                // recrusive call.
-                self::arrayToXml($value, $rootNodeName, $node);
-            }
-            else
-            {
-                // add single node.
-                $value = htmlentities($value);
-                $xml->addChild($key,$value);
-            }
-
-        }
-        // pass back as string. or simple xml object if you want!
-        return $xml->asXML();
+    public static function object2Array($object) {
+        $object =  json_decode( json_encode( $object),true);
+        return  $object;
     }
 }
