ba34d50662b0b3fc13facf9674c34221ee2a544f
axios <axioscros@aliyun.com>
2018-03-15 09:48:52 +0800

    ClientInterface

---
+++ library/core/lib/ClientInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/3/15 09:38
+ */
+
+namespace aliyun\sdk\core\lib;
+
+interface ClientInterface
+{
+    public static function action($action_name);
+
+    public function __construct($action_name);
+}
