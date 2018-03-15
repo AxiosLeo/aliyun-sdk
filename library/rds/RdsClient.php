d804a0f59a00414a0fe883d629f8bc1eda905172
axios <axioscros@aliyun.com>
2018-03-15 10:51:50 +0800

    rds

---
+++ library/rds/RdsClient.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/3/15 10:50
+ */
+
+namespace aliyun\sdk\rds;
+
+use aliyun\sdk\core\traits\ClientTrait;
+use aliyun\sdk\rds\request\RdsCommon;
+
+/**
+ * Class RdsClient
+ * Api Document : https://help.aliyun.com/document_detail/26226.html
+ * @package aliyun\sdk\rds
+ */
+class RdsClient extends RdsCommon
+{
+    use ClientTrait;
+}
---
+++ library/rds/request/RdsCommon.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/3/15 10:50
+ */
+
+namespace aliyun\sdk\rds\request;
+
+use aliyun\sdk\core\http\Request;
+
+class RdsCommon extends Request
+{
+    public function __construct()
+    {
+        $this->product = "rds";
+
+        parent::__construct();
+
+        $this->setParam("Version","2014-08-15");
+
+        $this->auth = false;
+    }
+}
