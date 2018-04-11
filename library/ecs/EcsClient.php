f2ee218b0c9a1cf0da34a944300d3e7522eff213
axios <axioscros@aliyun.com>
2018-04-11 10:12:33 +0800

    ecs client

---
+++ library/ecs/EcsClient.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/4/11 10:09
+ */
+
+namespace aliyun\sdk\ecs;
+
+use aliyun\sdk\core\traits\ClientTrait;
+use aliyun\sdk\ecs\request\EcsCommon;
+
+/**
+ * Class EcsClient
+ * API Document : https://help.aliyun.com/document_detail/25485.html
+ * @package aliyun\sdk\ecs
+ */
+class EcsClient extends EcsCommon
+{
+    use ClientTrait;
+}
