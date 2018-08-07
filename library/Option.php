0b5abf16180e546d08d59a0772e3661425226035
axios <axioscros@aliyun.com>
2018-08-07 14:00:50 +0800

    Options

--- library/Option.php
+++ library/Option.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @author: axios
- *
  * @email: axiosleo@foxmail.com
  * @blog:  http://hanxv.cn
  * @datetime: 2018/2/9 16:14
@@ -9,16 +8,42 @@
 
 namespace aliyun\sdk;
 
+use api\tool\lib\ArrayTool;
 
 class Option
 {
-    public static $option = [
-        'header_accept_lang'=>[
-            'zh-hans-cn' => 'zh-cn'
-        ]
-    ];
+    public static $option;
 
-    public static function get($index,$default = null){
-        return isset(self::$option[$index]) ? self::$option[$index] : $default;
+
+    protected static function options()
+    {
+        if (is_null(self::$option)) {
+            self::$option = ArrayTool::instance([
+                'header_accept_lang' => [
+                    'zh-hans-cn' => 'zh-cn'
+                ]
+            ]);
+        }
+        return self::$option;
+    }
+
+    public static function get($index = null, $default = null)
+    {
+        return self::options()->get($index, $default);
+    }
+
+    public static function set($index, $value = '')
+    {
+        return self::options()->set($index, $value);
+    }
+
+    public static function delete($index)
+    {
+        return self::options()->delete($index);
+    }
+
+    public static function list($index = '')
+    {
+        return self::options()->getChildKeyList($index);
     }
 }
