e138c02b5b6fa7dc6dd05ed48ad34f4e8a164659
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2019-04-23 14:33:41 +0800

    update example

--- example/alidns.php
+++ example/alidns.php
@@ -12,6 +12,7 @@ require_once __DIR__ . '/base.php';
 
 use aliyun\sdk\Aliyun;
 use aliyun\sdk\services\Alidns;
+use aliyun\sdk\services\AliyunClient;
 
 Aliyun::region('cn-hangzhou');
 
@@ -21,3 +22,9 @@ $response = Alidns::V20150109()->DescribeDomains()
     ->request();
 
 dump($response->getContent());
+
+// or
+AliyunClient::Alidns20150109()->DescribeDomains()
+    ->setPageNumber(1)
+    ->setPageSize(15)
+    ->request();
--- example/vod.php
+++ example/vod.php
@@ -9,6 +9,7 @@
 namespace aliyun\sdk\example;
 
 use aliyun\sdk\Aliyun;
+use aliyun\sdk\services\AliyunClient;
 use aliyun\sdk\services\Vod;
 
 require_once __DIR__ . '/base.php';
@@ -20,3 +21,8 @@ $response = Vod::V20170321()->GetCategories()
     ->request();
 
 dump($response->getContent());
+
+// or
+AliyunClient::Vod20170321()->GetCategories()
+    ->setCateId(-1)
+    ->request();
