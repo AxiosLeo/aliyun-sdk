0a6602802aa8caaf2574bee98554c6f262b9f226
axios <axioscros@aliyun.com>
2019-05-12 23:34:29 +0800

    update example code

--- example/apigateway.php
+++ example/apigateway.php
@@ -9,6 +9,7 @@
 namespace aliyun\sdk\example;
 
 use aliyun\sdk\Aliyun;
+use aliyun\sdk\services\AliyunServices;
 use aliyun\sdk\services\CloudAPI;
 
 require_once __DIR__ . '/base.php';
@@ -16,6 +17,14 @@ require_once __DIR__ . '/base.php';
 Aliyun::region('cn-hangzhou');
 
 $response = CloudAPI::V20160714()->CreateApi()
+    ->setGroupId(1)
+    ->setApiName("api_name")
+    ->setVisibility(1)
+    ->request();
+
+dump($response->getContent());
+
+$response = AliyunServices::CloudAPI20160714()->CreateApi()
     ->setGroupId(1)
     ->setApiName("api_name")
     ->setVisibility(1)
